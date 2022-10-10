8e581bfbb954a58bb458953609258a16d1e13c5e
Jan Kneissler <jan.kneissler@breuninger.de>
2022-10-10 12:07:39 +0200

    added markdown decomposing

--- src/main/java/org/jkube/gitbeaver/MarkdownDecomposer.java
+++ src/main/java/org/jkube/gitbeaver/MarkdownDecomposer.java
@@ -1,9 +1,97 @@
 package org.jkube.gitbeaver;
 
+import org.jkube.gitbeaver.util.FileUtil;
+import org.jkube.markdown.ElementType;
+import org.jkube.markdown.MarkdownElement;
+import org.jkube.markdown.MarkdownFile;
+import org.jkube.markdown.MarkdownSection;
+
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.jkube.logging.Log.onException;
 
 public class MarkdownDecomposer  {
 
+    private static final String INFO = "info";
+    private static final String SECTION = "section";
+    private static final String TITLE = "title";
+    private static final String LEVEL = "level";
+    private static final String ROW = "row";
+    private static final String SEP = " ";
+
     public void decompose(Path sourcePath, Path targetPath) {
+        MarkdownFile input = new MarkdownFile(sourcePath);
+        writeRecursively(targetPath, input.getMainSection());
+    }
+
+    private void writeRecursively(Path path, MarkdownSection section) {
+        FileUtil.createIfNotExists(path);
+        writeInfo(path.resolve(INFO), section.getTitle(), section.getLevel());
+        Map<ElementType, Integer> num = new HashMap<>();
+        section.getElements().forEach(element -> num.put(element.getType(), num.getOrDefault(element.getType(), 0)+1));
+        Map<ElementType, Integer> counts = new HashMap<>();
+        section.getElements().forEach(element -> writeElement(counts, num, path, element));
+        int numsections = section.getSubSections().size();
+        int i = 1;
+        for (MarkdownSection subsection : section.getSubSections()) {
+            writeRecursively(path.resolve(SECTION+getNumberSuffix(i++, numsections)), subsection);
+        }
+    }
+
+    private void writeInfo(Path file, String title, int level) {
+        List<String> info = List.of(TITLE+SEP+title, LEVEL+SEP+level);
+        onException(() -> Files.write(file, info))
+                .fail("Could not write info to "+file);
+    }
+
+    private void writeElement(Map<ElementType, Integer> counts, Map<ElementType, Integer> num, Path path, MarkdownElement element) {
+        ElementType type = element.getType();
+        int count = counts.getOrDefault(type, 0)+1;
+        counts.put(type, count);
+        String name = type.name().toLowerCase()+"-"+getNumberSuffix(count, num.get(type));
+        Path target = path.resolve(name);
+        if (type == ElementType.TABLE) {
+            writeTable(target, element.getLines());
+        } else {
+            writeLines(target, element.getLines());
+        }
+     }
+
+    private void writeTable(Path target, List<String> lines) {
+        List<Map<String, String>> rows = new MarkdownTableParser(lines).getRows();
+        FileUtil.createIfNotExists(target);
+        int i = 0;
+        for (Map<String, String> row : rows) {
+            writeRow(target.resolve(ROW+getNumberSuffix(++i, rows.size())), row);
+        }
+    }
+
+    private void writeRow(Path file, Map<String, String> row) {
+        List<String> lines = new ArrayList<>();
+        row.forEach((k,v) -> lines.add(k+SEP+v));
+        writeLines(file, lines);
+    }
+
+    private void writeLines(Path target, List<String> lines) {
+        onException(() -> Files.write(target, lines))
+                .fail("Could not write output lines to "+target);
+    }
+
+    private String getNumberSuffix(int value, int num) {
+        String res = Integer.toString(value);
+        int len = 0;
+        while (num > 0) {
+            num /= 10;
+            len++;
+        }
+        if (res.length() < len) {
+            res = "0".repeat(len - res.length());
+        }
+        return "-"+res;
     }
 }
---
+++ src/main/java/org/jkube/gitbeaver/MarkdownTableParser.java
@@ -0,0 +1,58 @@
+package org.jkube.gitbeaver;
+
+
+import org.jkube.util.Expect;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class MarkdownTableParser {
+
+	private static final Pattern TABLE_SEP_MATCHER = Pattern.compile(":?-+:?");
+
+	private final List<String> columnTitles;
+	private final List<Map<String, String>> rows;
+
+	public MarkdownTableParser(List<String> lines) {
+		Expect.atLeast(lines.size(), 2).elseFail("Expected at least 2 lines for table, got "+lines.size());
+		columnTitles = parseLine(lines.get(0));
+		rows = new ArrayList<>();
+		Expect.isTrue(TABLE_SEP_MATCHER.matcher(lines.get(1)).matches()).elseFail("expected markdown table separator");
+		for (int i = 2; i < lines.size(); i++) {
+			rows.add(parseRow(lines.get(i)));
+		}
+	}
+
+	public List<String> getColumnTitles() {
+		return columnTitles;
+	}
+
+	public List<Map<String, String>> getRows() {
+		return rows;
+	}
+
+	private Map<String, String> parseRow(String line) {
+		List<String> cells = parseLine(line);
+		Expect.size(cells, columnTitles.size()).elseFail("Row has "+cells.size()+" cells, table has "+columnTitles.size()+" columns");
+		Map<String, String> result = new LinkedHashMap<>();
+		for (int i = 0; i < cells.size(); i++) {
+			result.put(columnTitles.get(i), cells.get(i));
+		}
+		return result;
+	}
+
+	private List<String> parseLine(String line) {
+		if (line.startsWith("|")) {
+			line = line.substring(1);
+		}
+		if (line.endsWith("|")) {
+			line = line.substring(0, line.length()-1);
+		}
+		List<String> res = new ArrayList<>();
+		for (String s : line.split("\\|")) {
+			res.add(s.trim());
+		}
+		return res;
+	}
+
+}
